fd0bcae8250ce625bb7dcb4459c0e00b6203057d
IndianMax03 <vermamanu@mail.ru>
2025-03-16 01:10:48 +0300

    Add: #40: bot & #60: echo bot

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	env "github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Beroli is libero!")
+	err := env.Load()
+	if err != nil {
+		log.Fatal("Can't find .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+    if err != nil {
+        panic(err)
+    }
+
+    bot.Debug = true
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	fmt.Println("Started...")
+	
+	for update := range updates {
+
+		if update.Message == nil {
+            continue
+        }
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		if _, err := bot.Send(msg); err != nil {
+            panic(err)
+        }
+	}
+
 }
