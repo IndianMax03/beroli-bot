47f532cfad8e118551404cbe692ec06e213bc615
IndianMax03 <vermamanu@mail.ru>
2025-03-22 23:33:38 +0300

    #43: Add: my yandex tracker go client

--- go.mod
+++ go.mod
@@ -1,7 +1,15 @@
 module github.com/IndianMax03/beroli-bot
 
-go 1.23.2
+go 1.24.1
 
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/IndianMax03/yandex-tracker-go-client v1.0.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	golang.org/x/net v0.37.0 // indirect
+	resty.dev/v3 v3.0.0-beta.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
+	client "github.com/IndianMax03/yandex-tracker-go-client"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	env "github.com/joho/godotenv"
 )
@@ -50,16 +48,10 @@ func main() {
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = 30
 
-	client := http.Client{Timeout: 3 * time.Second}
-	uri := "https://api.tracker.yandex.net/v2/queues/"
-	req, err := http.NewRequest(http.MethodGet, uri, nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("OAuth %s", YANDEX_API_TOKEN))
-	req.Header.Add("X-Cloud-Org-Id", YANDEX_ORGANIZATION_ID)
+	c := client.New(YANDEX_API_TOKEN, YANDEX_ORGANIZATION_ID, "", "ru")
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
@@ -67,20 +59,18 @@ func main() {
 		if update.Message == nil || update.Message.From.UserName != ALLOWED_USERNAME {
 			continue
 		}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
+
 		var queues []Queue
-		json.Unmarshal(body, &queues)
-		result := fmt.Sprintf("Status: %s\nQueues:\n", resp.Status)
+		resp, err := c.SendRequest(http.MethodGet, "/queues/", nil, &queues)
+		if err != nil {
+			panic(err)
+		}
+
+		result := fmt.Sprintf("Status: %s\nQueues:\n", resp.Status())
 		for _, q := range queues {
 			result += fmt.Sprintf("ID = %v; Key = %s; Name = %s\n", q.Id, q.Key, q.Name)
 		}
+
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, result)
 		msg.ReplyToMessageID = update.Message.MessageID
 		resp.Body.Close()
