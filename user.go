967fe6d777725fd8c869e47ff1c9412b572c4d86
IndianMax03 <vermamanu@mail.ru>
2025-04-11 00:40:15 +0300

    Add: user validation & default user & user fields

--- user.go
+++ user.go
@@ -1,20 +1,45 @@
 package main
 
-import "github.com/IndianMax03/yandex-tracker-go-client/v3/model"
+import (
+	"errors"
+
+	"github.com/IndianMax03/yandex-tracker-go-client/v3/model"
+)
 
 const (
 	USERNAME_FIELD = "username"
 	STATE_FIELD    = "state"
+	ISSUE_FIELD    = "issue"
+	ISSUES_FIELD   = "issues"
+)
+
+var (
+	ErrMandatoryIsRequired = errors.New("mandatory fields is required")
 )
 
 type User struct {
-	Username string
-	State    string
-	Issue    *model.IssueCreateRequest
-	Issues   []Issue
+	Username string                    `bson:"username"`
+	State    string                    `bson:"state"`
+	Issue    *model.IssueCreateRequest `bson:"issue"`
+	Issues   []Issue                   `bson:"issues"`
 }
 
 type Issue struct {
 	Key  string
 	Link string
+}
+
+func NewDefaultIssue() *model.IssueCreateRequest {
+	return &model.IssueCreateRequest{
+		Queue: model.Queue{
+			Key: TRACKER_QUEUE,
+		},
+	}
+}
+
+func (u *User) validateRequest() error {
+	if u.Issue.Summary == "" || u.Issue.Queue.Key == "" {
+		return ErrMandatoryIsRequired
+	}
+	return nil
 }
