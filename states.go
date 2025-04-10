36e5ff5b9630319f267e5732e3eab39ad9d072ab
IndianMax03 <vermamanu@mail.ru>
2025-04-11 00:40:50 +0300

    Update: states

--- states.go
+++ states.go
@@ -1,9 +1,8 @@
 package main
 
 const (
-	NIL_STATE            = "nil"
-	CREATING_STATE       = "creating"
-	DONE_STATE           = "done"
-	CANCELED_STATE       = "canceled"
-	ERROR_CREATING_STATE = "error_creating"
+	NIL_STATE      = "nil"
+	CREATING_STATE = "creating"
+	DONE_STATE     = "done"
+	CANCELED_STATE = "canceled"
 )
