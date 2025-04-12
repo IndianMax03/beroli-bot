53fa34542ff526af36bc202fd924404a896969dc
IndianMax03 <vermamanu@mail.ru>
2025-04-13 02:27:45 +0300

    Add: states localized description

--- states.go
+++ states.go
@@ -1,8 +1,27 @@
 package main
 
+import "errors"
+
 const (
 	NIL_STATE      = "nil"
 	CREATING_STATE = "creating"
 	DONE_STATE     = "done"
 	CANCELED_STATE = "canceled"
 )
+
+var ErrStateNotExists = errors.New("state not exists")
+
+var localizedStatesDescriptionMap = map[string]string{
+	NIL_STATE:      "отсутствует",
+	CREATING_STATE: "создание задачи",
+	DONE_STATE:     "задача создана",
+	CANCELED_STATE: "задача отменена",
+}
+
+func GetLocalizedStateDescription(state string) (string, error) {
+	if result, ok := localizedStatesDescriptionMap[state]; !ok {
+		return "", ErrStateNotExists
+	} else {
+		return result, nil
+	}
+}
