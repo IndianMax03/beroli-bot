c897858e639ce3dfeca33da821314cce82123d1d
IndianMax03 <vermamanu@mail.ru>
2025-04-20 22:32:03 +0300

    Add: states.go tests

--- states.go
+++ states.go
@@ -3,19 +3,23 @@ package main
 import "errors"
 
 const (
-	NIL_STATE      = "nil"
-	CREATING_STATE = "creating"
-	DONE_STATE     = "done"
-	CANCELED_STATE = "canceled"
+	NIL_STATE         = "nil"
+	CREATING_STATE    = "creating"
+	DONE_STATE        = "done"
+	CANCELED_STATE    = "canceled"
+	NIL_STATE_RU      = "отсутствует"
+	CREATING_STATE_RU = "создание задачи"
+	DONE_STATE_RU     = "задача создана"
+	CANCELED_STATE_RU = "задача отменена"
 )
 
 var ErrStateNotExists = errors.New("неизвестное состояние пользователя")
 
 var localizedStatesDescriptionMap = map[string]string{
-	NIL_STATE:      "отсутствует",
-	CREATING_STATE: "создание задачи",
-	DONE_STATE:     "задача создана",
-	CANCELED_STATE: "задача отменена",
+	NIL_STATE:      NIL_STATE_RU,
+	CREATING_STATE: CREATING_STATE_RU,
+	DONE_STATE:     DONE_STATE_RU,
+	CANCELED_STATE: CANCELED_STATE_RU,
 }
 
 func GetLocalizedStateDescription(state string) (string, error) {
---
+++ states_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func genratePositiveCasesGetLocalizedStateDescription() []InputStringWantString {
+	cases := []InputStringWantString{
+		{
+			Input: NIL_STATE,
+			Want:  NIL_STATE_RU,
+		},
+		{
+			Input: CREATING_STATE,
+			Want:  CREATING_STATE_RU,
+		},
+		{
+			Input: DONE_STATE,
+			Want:  DONE_STATE_RU,
+		},
+		{
+			Input: CANCELED_STATE,
+			Want:  CANCELED_STATE_RU,
+		},
+	}
+
+	return cases
+}
+
+func genrateNegativeCasesGetLocalizedStateDescription() []InputStringWantError {
+	cases := []InputStringWantError{
+		{
+			Input: "",
+			Error: ErrStateNotExists,
+		},
+		{
+			Input: RandStringWithSymbolsAndEmojis(10),
+			Error: ErrStateNotExists,
+		},
+	}
+
+	return cases
+}
+
+func TestGetLocalizedStateDescriptionPositive(t *testing.T) {
+	tests := genratePositiveCasesGetLocalizedStateDescription()
+	for _, test := range tests {
+		name := fmt.Sprintf("CASE:'%s'->'%s'", test.Input, test.Want)
+		t.Run(name, func(t *testing.T) {
+			got, err := GetLocalizedStateDescription(test.Input)
+			if err != nil {
+				t.Errorf("Got error: '%v', Want '%s'", err, test.Want)
+			} else if got != test.Want {
+				t.Errorf("Got: '%s', Want '%s'", got, test.Want)
+			}
+		})
+	}
+}
+
+func TestGetLocalizedStateDescriptionNegative(t *testing.T) {
+	tests := genrateNegativeCasesGetLocalizedStateDescription()
+	for _, test := range tests {
+		name := fmt.Sprintf("CASE:'%s'->'%v'", test.Input, test.Error)
+		t.Run(name, func(t *testing.T) {
+			_, err := GetLocalizedStateDescription(test.Input)
+			if err != test.Error {
+				t.Errorf("Got: error is nil, Want error '%v'", test.Error)
+			}
+		})
+	}
+}
