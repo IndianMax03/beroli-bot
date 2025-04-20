e13253422dc7ff44d39406c000ad405e54e03007
IndianMax03 <vermamanu@mail.ru>
2025-04-20 21:27:02 +0300

    Add: tests general structure

---
+++ test_general.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	mrand "math/rand"
+	"strings"
+)
+
+const (
+	SEED     = 31415926
+	ALPHABET = "фбвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	SYMBOLS  = " +-='\"!@#$%^&*()[]{}_~`"
+	EMOJIS   = "😊😇😂😡😤😭😀"
+)
+
+var (
+	Rand                  *mrand.Rand
+	Alphabet              = []rune(ALPHABET)
+	AlphabetSymbols       = []rune(ALPHABET + SYMBOLS)
+	AlphabetSymbolsEmojis = []rune(ALPHABET + SYMBOLS + EMOJIS)
+)
+
+type InputStringWantString struct {
+	Input string
+	Want  string
+}
+
+type InputArrayOfStringWantString struct {
+	Input []string
+	Want  string
+}
+
+func init() {
+	Rand = mrand.New(mrand.NewSource(SEED))
+}
+
+func RandStringWithSymbolsAndEmojis(length int) string {
+	var b strings.Builder
+	b.Grow(length)
+	for range length {
+		b.WriteRune(AlphabetSymbolsEmojis[Rand.Intn(len(AlphabetSymbolsEmojis))])
+	}
+	return b.String()
+}
